3fcb00c85d3c7493b69fd6498c560e4919b75989
Joseph <joseph28-vasquez@hotmail.com>
2021-07-06 16:53:48 -0500

    modificar

--- load.js
+++ load.js
@@ -1,7 +1,6 @@
 if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("./sw.js")
-    .then(
-        reg => console.log("Registro Exitoso"))
-    .catch(
-        err => console.log(err));
-}
+    navigator.serviceWorker
+      .register("./sw.js")
+      .then((reg) => console.log("Registro Exitoso"))
+      .catch((err) => console.log(err));
+  }
